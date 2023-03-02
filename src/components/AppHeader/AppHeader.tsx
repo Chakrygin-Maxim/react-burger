67d0c5709669e9c7abd5db1ba70e0624254c0d20
Максим Д. Чакрыгин <chakrygin@yoomoney.ru>
2023-03-02 21:01:35 +0300

    add: добавить header

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,8 +1,34 @@
+import appHeaderStyle from './AppHeader.module.css'
+import {
+  BurgerIcon,
+  Logo,
+  ProfileIcon,
+} from '@ya.praktikum/react-developer-burger-ui-components'
+
 function AppHeader() {
   return (
-    <div>
-      <header>привет</header>
-    </div>
+    <header className={appHeaderStyle.appHeader}>
+      <nav className={appHeaderStyle.appHeader__navbar}>
+        <ul className={appHeaderStyle.appHeader__list}>
+          <li>
+            <a className={appHeaderStyle.appHeader__link}>
+              <BurgerIcon type="primary" />
+              <p className={appHeaderStyle.appHeader__linkText}>Конструктор</p>
+            </a>
+          </li>
+          <li>
+            <a className={appHeaderStyle.appHeader__link}>Лента Заказов</a>
+          </li>
+        </ul>
+        <div className={appHeaderStyle.appHeader__mainLogo}>
+          <Logo />
+        </div>
+        <a className={appHeaderStyle.appHeader__link}>
+          <ProfileIcon type="secondary" />
+          <p className={appHeaderStyle.appHeader__linkText}>Личный Кабинет</p>
+        </a>
+      </nav>
+    </header>
   )
 }
 
